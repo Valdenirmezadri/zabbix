91e53e6e58656b4d79cd242e6c4fc49556149f19
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2018-05-15 10:10:06 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+yum install zabbix zabbix-agent -y &&
+cd /tmp/ &&
+git clone https://github.com/Valdenirmezadri/zabbix &&
+rsync -zuva zabbix/ /etc/zabbix/ &&
+chmod +x /etc/zabbix/bin/* &&
+cp /etc/zabbix/sudoers.d/* /etc/sudoers.d/ &&
+read -p "Nome do Host: " host &&
+sed -i "s/^Hostname=.*/Hostname=$host/g" /etc/zabbix/zabbix_agentd.conf &&
+sed -i "s/^Server=.*/Server=177.37.95.190,186.209.30.113,177.54.11.234/29/g" /etc/zabbix/zabbix_agentd.conf &&
