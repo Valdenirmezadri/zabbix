a3a2d9139b1782e1374babe189e650ad13c3fcf1
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2018-05-15 10:12:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ chmod +x /etc/zabbix/bin/* &&
 cp /etc/zabbix/sudoers.d/* /etc/sudoers.d/ &&
 read -p "Nome do Host: " host &&
 sed -i "s/^Hostname=.*/Hostname=$host/g" /etc/zabbix/zabbix_agentd.conf &&
-sed -i "s/^Server=.*/Server=177.37.95.190,186.209.30.113,177.54.11.234/29/g" /etc/zabbix/zabbix_agentd.conf &&
+sed -i "s/^Server=.*/Server=177.37.95.190,186.209.30.113,177.54.11.234/29/g" /etc/zabbix/zabbix_agentd.conf
