2b00b6b8d821102074ffb27c2eab462891c7299e
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2018-05-16 10:10:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd /tmp/ &&
 git clone https://github.com/Valdenirmezadri/zabbix &&
 rsync -zuva zabbix/ /etc/zabbix/ &&
 chmod +x /etc/zabbix/bin/* &&
-cp /etc/zabbix/sudoers.d/* /etc/sudoers.d/ &&
+mv /etc/zabbix/zabbix_agentd.conf.d /etc/zabbix/zabbix_agentd.d &&
 read -p "Nome do Host: " host &&
 sed -i "s/^Hostname=.*/Hostname=$host/g" /etc/zabbix/zabbix_agentd.conf &&
 sed -i "s/^Server=.*/Server=177.37.95.190,186.209.30.113,177.54.11.234\/29/g" /etc/zabbix/zabbix_agentd.conf &&
